35c3d816d56de631431131075f88d0a6cc3a2707
James Hunt <james@niftylogic.com>
2021-06-05 15:11:24 +0000

    Support Project Filtering (by regex!)

--- ui/src/components/Board.vue
+++ ui/src/components/Board.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="board">
+    <input id="q" placeholder="filter by project name..." v-model="q" />
     <div class="waiting-on-me">
       <h2>Waiting on Me</h2>
       <div v-if="waiting.length == 0" class="no-data">
         You're not blocking anyone!  Go you! 🎉
       </div>
+      <div v-else-if="waitingTasks.length == 0" class="no-data">
+        No <em>matching</em> blockers found, so that's a start...
+      </div>
       <ul v-else>
-        <Task v-for="task in waiting" :task="task" :key="task.id" @updated="updated" @closed="closed"/>
+        <Task v-for="task in waitingTasks" :task="task" :key="task.id" @updated="updated" @closed="closed"/>
       </ul>
     </div>
 
@@ -15,28 +19,37 @@
       <div v-if="review.length == 0" class="no-data">
         <p>Nothing is out for review.<br>Time to dig into that backlog! 🎉</p>
       </div>
+      <div v-else-if="reviewTasks.length == 0" class="no-data">
+        <p>There <em>is</em> stuff out for review,<br>but not matching your filter criteria...</p>
+      </div>
       <ul v-else>
-        <Task v-for="task in review" :task="task" :key="task.id" @updated="updated" @closed="closed"/>
+        <Task v-for="task in reviewTasks" :task="task" :key="task.id" @updated="updated" @closed="closed"/>
       </ul>
     </div>
 
     <div class="free-and-clear">
       <h2>Free &amp; Clear</h2>
       <div v-if="free.length == 0" class="no-data">
-        The easy part is done.  Now go do some bizdev! 🎉
+        <p>The easy part is done.  Now go do some bizdev! 🎉</p>
+      </div>
+      <div v-else-if="freeTasks.length == 0" class="no-data">
+        <p>Nothing(-ish) is free and clear,<br>maybe try a different filter?</p>
       </div>
       <ul v-else>
-        <Task v-for="task in free" :task="task" :key="task.id" @updated="updated" @closed="closed"/>
+        <Task v-for="task in freeTasks" :task="task" :key="task.id" @updated="updated" @closed="closed"/>
       </ul>
     </div>
 
     <div class="blocked">
       <h2>Blocked</h2>
       <div v-if="blocked.length == 0" class="no-data">
-        No tasks are blocked by others.  Go team! 🎉
+        <p>No tasks are blocked by others.  Go team! 🎉</p>
+      </div>
+      <div v-else-if="blockedTasks.length == 0" class="no-data">
+        <p>No (matching) tasks are blocked by others.<br>That's good, right?</p>
       </div>
       <ul v-else>
-        <Task v-for="task in blocked" :task="task" :key="task.id" @updated="updated" @closed="closed"/>
+        <Task v-for="task in blockedTasks" :task="task" :key="task.id" @updated="updated" @closed="closed"/>
       </ul>
     </div>
   </div>
@@ -48,6 +61,11 @@ export default {
   name: 'Board',
   props: ['context', 'waiting', 'blocked', 'free', 'review'],
   components: {Task},
+  data() {
+    return {
+      q: ''
+    }
+  },
   methods: {
     updated(task) {
       this.$emit('updated', task)
@@ -55,9 +73,28 @@ export default {
     closed(task) {
       this.$emit('closed', task)
     }
+  },
+  computed: {
+    waitingTasks() {
+      return this.waiting.filter(t => this.q ? t.project.match(this.q) : true)
+    },
+    blockedTasks() {
+      return this.blocked.filter(t => this.q ? t.project.match(this.q) : true)
+    },
+    freeTasks() {
+      return this.free.filter(t => this.q ? t.project.match(this.q) : true)
+    },
+    reviewTasks() {
+      return this.review.filter(t => this.q ? t.project.match(this.q) : true)
+    }
   }
 }
 </script>
 
 <style>
+#q {
+  grid-column: 1/3;
+  font-size: 16pt;
+  padding: 1em 1.5em;
+}
 </style>
